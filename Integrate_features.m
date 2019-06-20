08ce5844b1b3c9e79dd7656e0bc9f304a2ba76c4
TheOldGogo <julien.gorenflot@kaust.edu.sa>
2019-06-20 10:37:54 +0300

    Bug fix: Integrate_features was sometimes sending an error -> fixed

--- Integrate_features.m
+++ Integrate_features.m
@@ -9,10 +9,10 @@ function out = Integrate_features(spect_struc,wavelength,lim)
 for i = 1:size(spect_struc,2)                                              % take one fluence...
     for k = 1:size(spect_struc(i).spectra,2)                                        % one time delay range
         limits = find(diff(sign(spect_struc(i).spectra(1:find(wavelength>=lim,1),k))));                            % find where the signs changes
-        temp = [1,limits';limits',find(wavelength>=lim,1)];
-        fluence(i).delay(k).features = wavelength(temp);
+        temp = [1,limits';limits',find(wavelength>=lim,1)]
+        fluence(i).delay(k).features = wavelength(temp)
         for j = 1:size(temp,2)
-            fluence(i).delay(k).integral(j) = trapz(wavelength(temp(1,j):temp(2,j)),spect_struc(i).spectra(temp(1,j):temp(2,j),k));
+            fluence(i).delay(k).integral(j) = trapz(wavelength(temp(1,j):temp(2,j)),spect_struc(i).spectra(temp(1,j):temp(2,j),k),1);
         end
         fluence(i).delay(k).PB = sum(fluence(i).delay(k).integral(fluence(i).delay(k).integral>=0));
     end
